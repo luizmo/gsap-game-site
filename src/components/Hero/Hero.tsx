de80c790fc65db07f1d14521b5a57178e94ac934
Luiz Miguel <luizmiguel.sistemas@gmail.com>
2024-12-06 18:50:36 -0300

    chore: remove unused imports

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 import { ScrollTrigger } from 'gsap/all'
